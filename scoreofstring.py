5267e90d2d42a1692b04263a73e946f8f5a6a6a9
06ergin06 <ibrahimh.ergin@gmail.com>
2025-02-17 22:29:54 +0300

    1 problem added

--- scoreofstring.py
+++ scoreofstring.py
@@ -1,8 +1,8 @@
 # https://leetcode.com/problems/score-of-a-string/description/
-def scoreOfString(s):
-    y = 0
-    for i in range(0,(len(s) - 1)):
-        y += abs(ord(s[i]) - ord(s[i+1])) 
+class Solution(object):
+    def scoreOfString(self, s):
+        y = 0
+        for i in range(0,(len(s) - 1)):
+            y += abs(ord(s[i]) - ord(s[i+1])) 
         
-    return y
-print(scoreOfString("hello"))
+        return y
