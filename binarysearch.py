dfcbc361e7439b5a59d54b66f4df9191ea65cf5c
06ergin06 <ibrahimh.ergin@gmail.com>
2025-02-20 20:35:59 +0300

    remove unnecessary if

--- binarysearch.py
+++ binarysearch.py
@@ -3,15 +3,14 @@ class Solution(object):
     def search(self, nums, target):
         low = 0
         high = len(nums) - 1
-        if(high >= low):
-            while(high >= low):
-                mid = (low + high) / 2 
-                if target == nums[mid]:
-                    return mid
-                elif nums[mid] > target:
-                    high = mid - 1 
-                elif target > nums[mid]:
-                    low = mid + 1
-                else: 
-                    return -1
+        while high >= low:
+            mid = (low + high) / 2
+            if target == nums[mid]:
+                return mid
+            elif nums[mid] > target:
+                high = mid - 1
+            elif target > nums[mid]:
+                low = mid + 1
+            else:
+                return -1
         return -1
