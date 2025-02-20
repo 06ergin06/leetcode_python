1d2292aa0c0591aa90c885c93a7dbfcce1c499e9
06ergin06 <ibrahimh.ergin@gmail.com>
2025-02-20 21:29:06 +0300

    +=

--- divisiblevsnondivisible.py
+++ divisiblevsnondivisible.py
@@ -4,7 +4,7 @@ class Solution(object):
         num2 = 0
         for i in range (0,n + 1):
             if i%m == 0:
-                num2 = num2 + i
+                num2 += i
             else: 
-                num1 = num1 + i
+                num1 += i
         return num1 - num2
