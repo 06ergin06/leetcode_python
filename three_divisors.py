5ef446c5dc9424c27d25527b3f22de2c40b2aaa6
06ergin06 <ibrahimh.ergin@gmail.com>
2025-02-21 17:17:15 +0300

    1 problem added

---
+++ three_divisors.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/three-divisors/description/
+class Solution(object):
+    def isThree(self, n):
+        y = 0
+        for i in range(1,n+1):
+            if n % i == 0:
+                y += 1
+            if y == 4:
+                break
+        if y == 3:
+            return True
+        else: 
+            return False
