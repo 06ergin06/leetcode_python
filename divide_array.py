103750654ed5aa94fec9cc40be363c0c34f8ef04
06ergin06 <ibrahimh.ergin@gmail.com>
2025-03-17 22:13:24 +0300

    1 daily problem added

---
+++ divide_array.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def divideArray(self, nums):
+        if len(nums) % 2 != 0:
+            return False
+        nums = sorted(nums)
+        for i in range(0,len(nums) + 1,2):
+            if i < len(nums) and nums[i] == nums[i+1]:
+                continue
+            else:
+                return False
+        return True
